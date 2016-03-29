3c3ce21e70d05473205721b5a702c345db0b8e86
Zoochini <maxime.bardiot@gmail.com>
2016-03-29 14:48:20 +0200

    Nouvelle Architecture à compléter et à peaufiner

--- Humain.java
+++ Humain.java
@@ -1,19 +1,7 @@
+/**
+ * Created by Maxime on 29/03/2016.
+ */
+public interface Humain {
 
-import java.util.*;
-
-public class Humain extends Joueur{
-	
-	public Humain (String nom){
-		super(nom);
-	}
-	
-	public void jouer(int[][] jeu){
-		Scanner in = new Scanner(System.in);
-		System.out.println("Entrez le numero d'une ligne o� placer un trait");
-		int numl=in.nextInt();
-		System.out.println("Entrez le num�ro d'une colonne o� placer un trait");
-		int numc=in.nextInt();
-		jeu[numl][numc]=1;
-		in.close();
-	}
+    public void jouer();
 }
