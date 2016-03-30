a4c92417e1ee47cde37e5593bac854bace3812b9
Zoochini <maxime.bardiot@gmail.com>
2016-03-30 18:00:49 +0200

    Nouvelle Architecture à compléter et à peaufiner

--- Partie.java
+++ Partie.java
@@ -5,23 +5,88 @@ public class Partie {
 
 	public static void main(String[] args){
 		
-		Scanner in = new Scanner(System.in);
-		
-		//Définition du jeu/plateau
-		//Jeu jeu = new Jeu(args[1], Integer.parseInt(args[2]), Integer.parseInt(args[3]));
-		
-		Jeu jeu = new Jeu("rempli",3,3);
+		switch(args[0]){
 
-		Graphe g = new GraphSimple("r",jeu);
+            case "-name" :
+                System.out.println("Bardiot Maxime\nKhlifi Hicham\nBaccon Clément\nMagnougnet Cyril\n");
+                break;
 
-		/*
-		Joueur j1 = new Humain("Humain");
-		Joueur j2 = new Simplet();
-		*/
+            case "-h" :
+                System.out.println("Options duu programme : \n -name : affiche les noms et prénoms des personnes du groupe" +
+                        "\n -h : Rappelle la liste des options du programme\n -graphe type nblignes nbcolonnes : renvoie le graphe des config au format dot" +
+                        "\n -joue strategie : offre la possibilité de jouer sur la console de manière interactive" +
+                        "\n -cal stratégie : calcule une stratégie optimum face à un joueur suivant la stratégie passé en paramètre (renvoie le résultat au format pip)" +
+                        "\n -cal -graphe strategie : Même chose renvoyé sou forme dot\n -append parametres strategie : calcule une stratégie par méthode d'apprentissage" +
+                        "\n");
+                break;
 
-		jeu.afficher();
-		jeu.afficher1();
+            case "-graphe" :
+                int nblignes = Integer.parseInt(args[2]);//Faire fonctions définir jeu
+                int nbcolonnes = Integer.parseInt(args[3]);
+                Jeu j = DefJeu(args[1],args[2],args[3]);
+                Graphe g = new Graphe(j);
+                g.toDot();
+                break;
 
-		in.close();
+            case "-joue" :
+                Humain h = new Joueur();
+                switch(args[1]){
+                    case "-simplet" :
+                        IA s = new Simplet();
+                        Jeu j = DefJeu(args[2],args[3],args[4]);
+                        j.jouer(h,s);
+                        break;
+
+                    case "-prevoyant" :
+                        IA p = new Prevoyant();
+                        Jeu j = DefJeu(args[2],args[3],args[4]);
+                        j.jouer(h,p);
+                        break;
+
+                    case "-idiot" :
+                        IA i = new idiot();
+                        Jeu j = DefJeu(args[2],args[3],args[4]);
+                        j.jouer(h,i);
+                        break;
+
+                    //CAS A PART ATTENTION UTILISATION D'UN FICHIER PIP!!!
+
+                    case "-pondere" :
+                        IA po = new Pondere();
+                        Jeu j = DefJeu(args[2],args[3],args[4]);
+                        j.jouer(h,po);
+                        break;
+
+                    //FIN DU CAS
+
+                    default :
+                        System.out.println("Mauvais parametre, réessayer avec de bons paramètres");
+                        break;
+                }
+                break;
+
+            case "-cal" :
+                //à compléter
+                break;
+
+            case "-apprend" :
+                //à compléter
+                break;
+
+            case "-eval" :
+                //àcompléter
+                break;
+
+            case "-simul" :
+                //à compléter
+                break;
+		}
 	}
+
+    public static Jeu DefJeu(String argType,String argNblignes, String argNbcolonnes){
+        int nblignes = Integer.parseInt(argNblignes);//Faire fonctions définir jeu
+        int nbcolonnes = Integer.parseInt(argNbcolonnes);
+        Jeu j = new Jeu(argType, nblignes, nbcolonnes);
+        return j;
+    }
 }
